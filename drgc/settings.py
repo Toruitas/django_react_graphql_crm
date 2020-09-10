b8707be24c45a43c6bed689ee4aa87c9dd2f9938
Stuart Leitch <Toruitas@gmail.com>
2020-09-10 19:06:34 +0100

    Frontend app created, index loading OK.

--- drgc/settings.py
+++ drgc/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'graphene_django',
-    'customers'
+    'customers',
+    'frontend'
 ]
 
 MIDDLEWARE = [
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: [path.resolve(__dirname, 'frontend/src/index.js')],
+    output: {
+        path: path.resolve(__dirname, "frontend/static/frontend/public/"),
+        publicPath: "/static/frontend/public/",
+        filename: 'main.js',  // the same one we import in index.html
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|mjs)?$/,
+                exclude:  /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {presets: ["@babel/env", "@babel/preset-react"]}
+                },
+            },
+            {
+                test: /\.(png|jp?g|gif|svg)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              }
+        ],
+    },
+};
